d8480d71580a25892f15c54eaa8d8b8033472bdc
Shlok Gandhi <shlok.gandhi@gmail.com>
2016-05-20 20:48:39 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,72 @@
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user')
+var http = require('http');
+var path = require('path');
+
+var CUSTOMCONNSTR_MONGOLAB_URI = 'mongodb://localhost/chatroom';
+
+var app = express();
+var mongo = require('mongodb').MongoClient;
+
+app.set('port', process.env.PORT || 8000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+	app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+var serve = http.createServer(app);
+var io = require('socket.io')(serve);
+
+serve.listen(app.get('port'), function(){
+	console.log('Express server listening to port: ' + app.get('port'));
+});
+
+
+io.on('connection', function (socket) {
+	console.log('a user connected');
+
+	mongo.connect(CUSTOMCONNSTR_MONGOLAB_URI, function (err, db) {
+		if(err){
+			console.warn(err.message);
+		} else {
+			var collection = db.collection('chat_messages')
+			var stream = collection.find().sort().limit(10).stream();
+			stream.on('data', function (chat) { console.log('emitting chat'); socket.emit('chat', chat.content, chat.date); });
+		}
+	});
+
+	socket.on('disconnect', function () {
+		console.log('user disconnected');
+	});
+
+	socket.on('chat', function (msg, dt) {
+		mongo.connect(CUSTOMCONNSTR_MONGOLAB_URI, function (err, db) {
+			if(err){
+				console.warn(err.message);
+			} else {
+				var collection = db.collection('chat_messages');
+				collection.insert({ content: msg, date: dt }, function (err, o) {
+					if (err) { console.warn(err.message); }
+					else { console.log("chat message inserted into db: " + msg + "\nAnd o: " + o); }
+				});
+			}
+		});
+
+		socket.broadcast.emit('chat', msg);
+	});
+});
---
+++ public/js/index.js
@@ -0,0 +1,21 @@
+﻿var socket = io();
+$('#send-message-btn').click(function () {
+	var msg = $('#message-box').val();
+	var dt = new Date();
+	socket.emit('chat', msg, dt);
+	$('#messages').append(chat(msg,dt));
+	$('#message-box').val('');
+	var $target = $('html,body'); 
+	$target.animate({scrollTop: $target.height()}, 1000);
+	return false;
+});
+socket.on('chat', function (msg, date) {
+	var d = new Date(date);
+	$('#messages').append(chat(msg,d));
+});
+
+function chat(msg,dt){
+	var c = $('<li class="other">');	
+	c.append('<div class="msg">' + '<p>' + msg + '</p>' + '<time>' + jQuery.timeago(dt) + '</time></div></li>');
+	return c;
+}
